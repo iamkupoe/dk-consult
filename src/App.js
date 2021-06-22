99b2d29a7fc632c8e5393020c9fedc24fed38c2c
iamkupoe <samuelkupoe1@gmail.com>
2021-06-22 16:29:57 +0800

    title updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Hamburger from "hamburger-react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
